160d96f0b29bf811ad6caf99c9f90d02fc448646
Harold Hopson <hdhopson88@yahoo.com>
2020-08-23 15:56:29 -0700

    implemented GetAllStockSymbols action

--- Controllers/IEXController.cs
+++ Controllers/IEXController.cs
@@ -1,11 +1,32 @@
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StockJockAPI.Domain.Factories;
+using StockJockAPI.Domain.Models;
 
 namespace StockJockAPI.Controllers
 {
+    [Route("api/iex")]
+    [ApiController]
     public class IEXContorller : ControllerBase
     {
+        public IEXContorller()
+        {
+            _stockFactory = new StockFactory();
+        }
         
+        private readonly StockFactory _stockFactory;
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllStockSymbols()
+        {
+            ApiHelper.InitializeClient();
+
+            List<StockSymbol> symbols = await _stockFactory.LoadSymbols();
+
+            return Ok(symbols);
+        }
     }
 }
